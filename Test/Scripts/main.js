29fbe726cd28458a01a716e2d15f1e8f4e24779b
saitou.shusaku@gmail.com <saitou.shusaku@gmail.com>
2014-09-19 04:49:49 +0900

    １ページ目少し

--- Test/Scripts/main.js
+++ Test/Scripts/main.js
@@ -1,39 +1,34 @@
 ﻿$(function () {
-    var canvas = document.getElementById('c1');
-    if (typeof G_vmlCanvasManager != 'undefined') {
+    var canvas = document.getElementById("image_panel");
+    if (typeof G_vmlCanvasManager != "undefined") {
         canvas = G_vmlCanvasManager.initElement(canvas);
     }
     if (!canvas || !canvas.getContext) { return false; }
-    var ctx = canvas.getContext('2d');
+    var ctx = canvas.getContext("2d");
     var img = new Image();
     img.src = "Content/Images/images.gif";
     img.onload = function () {
-        ctx.drawImage(img, 5400, 0, 600, 500, 0, 0, 600, 500);
+        ctx.drawImage(img, 0, 0, 600, 500, 0, 0, 600, 500);
     }
     canvas.width = 600;
     canvas.height = 500;
-
-    $(canvas).mousemove(function (e) {
-        var x = e.pageX - $(this).offset().left;
-        if (x > 500) {
-            ctx.drawImage(img, 0, 500, 600, 500, 0, 0, 600, 500);
-            document.body.style.cursor = "pointer";
-        } else if (x > 400) {
-            ctx.drawImage(img, 0, 1000, 600, 500, 0, 0, 600, 500);
-            document.body.style.cursor = "move";
-        } else if (x > 300) {
-            ctx.drawImage(img, 0, 1500, 600, 500, 0, 0, 600, 500);
-            document.body.style.cursor = "wait";
-        } else if (x > 200) {
-            ctx.drawImage(img, 5400, 500, 600, 500, 0, 0, 600, 500);
-        } else if (x > 100) {
-            ctx.drawImage(img, 5400, 1000, 600, 500, 0, 0, 600, 500);
-        } else if (x > 0) {
-            ctx.drawImage(img, 5400, 1500, 600, 500, 0, 0, 600, 500);
-        }
-    });
-    $(canvas).mouseout(function () {
-        ctx.drawImage(img, 0, 0, 600, 500, 0, 0, 600, 500);
-        document.body.style.cursor = "auto";
-    });
+    events(ctx, img);
 });
+
+function events(ctx, img) {
+    // -------------------------------------------------------
+    // game_play_button
+    $("#game_play_button").hover(function () {
+        ctx.drawImage(img, 0, 1000, 600, 500, 0, 0, 600, 500);
+    }, function () {
+        ctx.drawImage(img, 0, 0, 600, 500, 0, 0, 600, 500);
+    });
+
+    // -------------------------------------------------------
+    // sound_stop_button
+    $("#sound_stop_button").hover(function () {
+        ctx.drawImage(img, 0, 500, 600, 500, 0, 0, 600, 500);
+    }, function () {
+        ctx.drawImage(img, 0, 0, 600, 500, 0, 0, 600, 500);
+    });
+}

8732373bcb68fc8a83a55015c67e4e5c97494a64
saitou.shusaku@gmail.com <saitou.shusaku@gmail.com>
2014-09-24 02:03:12 +0900

    -

--- Test/Scripts/main.js
+++ Test/Scripts/main.js
@@ -3,19 +3,17 @@ var _context = null;
 
 $(function () {
     $("#ver").text("5");
-    $("button").click(function () {
-        _img.src = "Content/Images/images2.gif";
-        _img.onload = function () {
-            alert("in!");
-            var canvas = document.getElementById("image_panel");
-            if (!canvas || !canvas.getContext) { return false; }
-            _context = canvas.getContext("2d");
-            canvas.width = 600;
-            canvas.height = 500;
-            setBackgroundImage(0, 0);
-            alert("out!");
-        }
-    });
+    _img.src = "Content/Images/images1.gif";
+    _img.onload = function () {
+        alert("in!");
+        var canvas = document.getElementById("image_panel");
+        if (!canvas || !canvas.getContext) { return false; }
+        _context = canvas.getContext("2d");
+        canvas.width = 600;
+        canvas.height = 500;
+        setBackgroundImage(0, 0);
+        alert("out!");
+    }
 });
 
 function setBackgroundImage(x, y) {

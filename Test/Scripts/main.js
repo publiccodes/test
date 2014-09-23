342699bdb6c2e29551c8d7d89a72e7565ff92cbf
saitou.shusaku@gmail.com <saitou.shusaku@gmail.com>
2014-09-24 01:56:30 +0900

    -

--- Test/Scripts/main.js
+++ Test/Scripts/main.js
@@ -2,18 +2,18 @@
 var _context = null;
 
 $(function () {
-    $("#ver").text("2");
+    $("#ver").text("3");
     $("button").click(function () {
         _img.src = "Content/Images/images2.gif";
         _img.onload = function () {
+            alert("in!");
             var canvas = document.getElementById("image_panel");
             if (!canvas || !canvas.getContext) { return false; }
             _context = canvas.getContext("2d");
             canvas.width = 600;
             canvas.height = 500;
-            alert("in");
             setBackgroundImage(0, 0);
-
+            alert("out!");
         }
     });
 });
